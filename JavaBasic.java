7c5361744f3270b4a87756abde04ee6158c9a90e
MD ALI UDDIN <mdali.uddinn@gmail.com>
2025-11-06 01:46:10 +0530

    Variables in Java

--- JavaBasic.java
+++ JavaBasic.java
@@ -2,12 +2,25 @@ public class JavaBasic {
     public static void main(String args[]) {
 
         //Code of Print Pattern using ln
-        System.out.println("****");
-        System.out.println("***");
-        System.out.println("**");
-        System.out.println("*");
+        // System.out.println("****");
+        // System.out.println("***");
+        // System.out.println("**");
+        // System.out.println("*");
 
         //2nd method of print pattern using \n
-        System.out.print("**** \n*** \n** \n*");
+        //System.out.print("***** \n**** \n*** \n** \n*");
+
+        
+         // Code of Variables in Java...
+        int a = 10;
+        int b = 5;
+        System.out.println(a);
+        System.out.println(b);
+        
+        String name = "Ali";
+        System.out.println (name);
+
+        a = 20;
+        System.out.println(a);
     }
 }
