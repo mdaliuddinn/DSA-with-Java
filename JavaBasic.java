30dabad428e8745264acce43914ff3c7649551b5
MD ALI UDDIN <mdali.uddinn@gmail.com>
2025-11-06 00:10:17 +0530

    Initial commit

---
+++ JavaBasic.java
@@ -0,0 +1,13 @@
+public class JavaBasic {
+    public static void main(String args[]) {
+
+        //Code of Print Pattern using ln
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+
+        //2nd method of print pattern using \n
+        System.out.print("**** \n*** \n** \n*");
+    }
+}
